c9557a40cf40408342128a6230aefcf992681198
LuizHenrique <luizhenrique9x08@gmail.com>
2022-06-09 22:12:22 -0300

    Atualizado

---
+++ js/busca.js
@@ -0,0 +1,16 @@
+let input = document.getElementById('barra_pesquisa')
+let saida = document.querySelector('div.busca_feita')
+function buscar_produto() {
+    let receber_input = input.value
+    if((receber_input != '') && (receber_input != null) && (receber_input != undefined)){
+        let valorBuscado = `
+        <div class="resultado_busca">
+            <p class="saida_de_dados">
+                ${receber_input}
+            </p>
+        </div>`
+
+        saida.innerHTML = valorBuscado
+        input.value = ''
+    }
+}
